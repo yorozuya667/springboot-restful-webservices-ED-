f6602af94fd41cb583aa1366aa6f2fec09c8de37
yorozuya667 <yorozuya667@gmail.com>
2025-09-29 11:25:31 +0500

    Lesson 82-84

--- src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
+++ src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
@@ -5,7 +5,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+    private String resourceName;
+    private String fieldName;
+    private Long fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue){
         super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+
     }
+
+
 }
